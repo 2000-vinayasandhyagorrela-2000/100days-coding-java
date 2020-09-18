ee8327cb2db7b3284b658aa05dcfb527c4d3f651
2000-vinayasandhyagorrela-2000 <69506701+2000-vinayasandhyagorrela-2000@users.noreply.github.com>
2020-09-18 18:00:52 +0530

    Add files via upload

---
+++ Rare_num.java
@@ -0,0 +1,41 @@
+package programs;
+import java.util.*;
+public class Rare_num
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number :");
+		int n = sc.nextInt(); // n=65
+		if (rare(n))  
+		System.out.println("Rare Number");
+		else
+			System.out.println("Not Rare Number");
+	}	
+	static int revdigits(int num) 
+	{
+		int rev =0;
+		while(num>0) 
+		{
+			int rem = num%10;  
+			rev = rev*10 + rem; 
+			num = num/10; 
+		}
+		return rev; 
+	}
+	static boolean rare(int N)
+	{
+		int revN = revdigits(N);
+		if( revN == N) 
+			 return false;
+		return sq(N+ revN)  && sq(N-revN);		
+		
+	}
+	
+	static boolean sq(double x)
+	{
+		double sr = Math.sqrt(x);
+		return (( sr - Math.floor(sr)) == 0);
+	}
+
+}
