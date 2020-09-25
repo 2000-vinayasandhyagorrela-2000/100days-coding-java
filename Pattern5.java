bccd106115f727f458831150e8cbcdb86f7d57db
2000-vinayasandhyagorrela-2000 <69506701+2000-vinayasandhyagorrela-2000@users.noreply.github.com>
2020-09-25 19:16:19 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,19 @@
+package pattern;
+import java.util.*;
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char i;
+		char j;
+		for( i ='A'; i<='E';i++) 
+		{
+			for( j='A'; j<='E';j++) 
+			{
+				System.out.print(j); 
+			}
+			System.out.println();
+		}
+	}
+
+}
