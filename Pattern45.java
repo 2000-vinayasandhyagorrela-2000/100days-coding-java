5c92cd2c8678c210e9be50ad9e3bc023ad333200
2000-vinayasandhyagorrela-2000 <69506701+2000-vinayasandhyagorrela-2000@users.noreply.github.com>
2020-10-12 18:08:07 +0530

    Add files via upload

---
+++ Pattern45.java
@@ -0,0 +1,16 @@
+package pattern2;
+
+public class Pattern45 {
+	public static void main(String[] args) {
+		int n = 3;
+					
+		for(int i=n; i >= -n; i--) 
+			{
+				for(int j= Math.abs(i); j <= n ; j++)
+			
+				System.out.print((char)(j+65)+" ");
+						
+				System.out.println();					
+			}
+}
+}
