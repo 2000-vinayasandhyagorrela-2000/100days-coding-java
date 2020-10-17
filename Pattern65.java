6d3a81137a1c618bafc5e0535f4eb1c9f3fc4d75
2000-vinayasandhyagorrela-2000 <69506701+2000-vinayasandhyagorrela-2000@users.noreply.github.com>
2020-10-17 18:32:57 +0530

    Add files via upload

---
+++ Pattern65.java
@@ -0,0 +1,23 @@
+package pattern3;
+
+public class Pattern65 {
+	public static void main(String[] args) {
+		int n = 4;
+		int x=1;
+		int j;
+		
+		for(int i=n; i>= -n; i--)
+		{
+		  for( j=1 ; j <= Math.abs(i); j++) 
+			System.out.print(" ");
+		  x = (i>0)?1:j;  
+		  
+		  for(int k= n; k>=Math.abs(i); k--)
+		  {
+			  System.out.print(x+" "); 
+		      x=x+1;
+		  }
+		  System.out.println();
+		}
+}
+}
